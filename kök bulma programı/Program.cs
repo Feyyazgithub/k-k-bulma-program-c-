b2cd9e3b0e9903caf4c8b45dab9a13a32773548c
Feyyaz Kizilgunes <feyyaz.remington@gmail.com>
2022-07-22 16:00:11 +0300

    Add project files.

---
+++ kök bulma programı/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace kök_bulma_programı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kökünü bulmak istediğiniz sayıyı giriniz = ");
+            double kok =double.Parse(Console.ReadLine());
+            double g = 1;
+            int i = 0;
+        a:
+            if ((g * g) != kok && i != 100)
+            {
+                g = (g + kok / g) / 2;
+                Console.WriteLine(g);
+                i++;
+                goto a;
+
+            }
+            Console.WriteLine("kök değeri = " + g);
+        }
+    }
+}
